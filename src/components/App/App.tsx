cff80fde76ec8150618e54d50287e4807ef38702
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-05-23 22:48:54 +0300

    loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,24 +3,28 @@ import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = (query: string) => {
     setMovies([]);
+    setIsLoading(true);
 
     fetchMovies(query).then((data) => {
       if (data.results.length === 0) {
         toast("No movies found for your request.");
       }
       setMovies(data.results);
+      setIsLoading(false);
     });
   };
 
-  const handleSlectMovie = (movie: Movie) => {
+  const handleSelectMovie = (movie: Movie) => {
     console.log("Select movie:", movie);
   };
 
@@ -28,8 +32,9 @@ function App() {
     <>
       <Toaster position="top-right" />
       <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
       {movies.length > 0 && (
-        <MovieGrid movies={movies} onSelect={handleSlectMovie} />
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
       )}
     </>
   );
