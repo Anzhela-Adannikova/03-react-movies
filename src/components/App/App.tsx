c51dcb06b119009cef35bd7a5be6fbd1ffee4380
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-05-23 23:21:25 +0300

    add ErrorMessage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,27 +4,34 @@ import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const handleSearch = (query: string) => {
+  const handleSearch = async (query: string) => {
     setMovies([]);
     setIsLoading(true);
 
-    fetchMovies(query).then((data) => {
+    try {
+      const data = await fetchMovies(query);
+
       if (data.results.length === 0) {
         toast("No movies found for your request.");
       }
+
       setMovies(data.results);
-      setIsLoading(false);
-    });
+    } catch {
+      setIsError(true);
+    }
+    setIsLoading(false);
   };
 
-  const handleSelectMovie = (movie: Movie) => {
+  const handleSelectMovie = async (movie: Movie) => {
     console.log("Select movie:", movie);
   };
 
@@ -33,6 +40,7 @@ function App() {
       <Toaster position="top-right" />
       <SearchBar onSubmit={handleSearch} />
       {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       {movies.length > 0 && (
         <MovieGrid movies={movies} onSelect={handleSelectMovie} />
       )}
